1ecb4b2e415e3d5765951391de3086d37d4afee9
Otavio Alves <otavioalvescbp@gmail.com>
2023-04-03 21:49:12 -0300

    Aula 03/04/23

---
+++ Lógica de Programacao (Dorival)/Aula_03_04_23/tabelaverdade.py
@@ -0,0 +1,28 @@
+vetorPossibilidade = [True, False]
+
+print("Testador de Fórmulas Tabajara 1.0")
+print("(A ^ B) v (¬A ^ ¬B)")
+
+for a in vetorPossibilidade:
+    for b in vetorPossibilidade:
+        if (a and b) or (not a and not b):
+            resultadoF=True
+        else:
+            resultadoF=False
+        print(f'A = {a} \t B = {b} \t H: {resultadoF}.')
+
+print('='*49)
+
+print('Fórmula 2: (A <-> B) ^ ¬(A v B)')
+
+linhas = 0
+
+for a in vetorPossibilidade:
+    for b in vetorPossibilidade:
+        if (a == b) and not (a or b) :
+            resultadoF=True
+        else:
+            resultadoF=False
+        print(f'A = {a} \t B = {b} \t Fórmula: {resultadoF}.')
+        linhas+=1
+print(f'A tabela tem {linhas} linhas.')
