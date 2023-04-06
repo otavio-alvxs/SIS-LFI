e9b754203e142ba6926a57c51b7502dd0d5fa735
Otavio Alves <otavioalvescbp@gmail.com>
2023-04-05 21:45:32 -0300

    LISTA LP

---
+++ Lab (Alysson)/Aula_15_03_23/Exercicios/Lista 01/ex05.py
@@ -0,0 +1,4 @@
+"""Faça um algoritmo que receba dois números e ao final mostre a soma, subtração, multiplicação e a divisão dos
+números lidos."""
+A = float(input('Digite o valor de A : '))
+B = float(input('Digite o valor de B: '))
