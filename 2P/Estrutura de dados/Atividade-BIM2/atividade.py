969b74f8573e2666b000662977e36ce21cafce3b
otavio-alvxs <otavioalvescbp@gmail.com>
2023-10-24 18:33:09 -0300

    finalização

--- 2P/Estrutura de dados/Atividade-BIM2/atividade.py
+++ 2P/Estrutura de dados/Atividade-BIM2/atividade.py
@@ -1,8 +1,14 @@
-def retornomenu():
-    resp = input('Deseja realizar a operação novamente (S para sim e qualquer outra coisa para voltar ao menu)?')
+def retornomenu(a):
+    resp = input('Deseja realizar a operação novamente (S para sim e qualquer outra coisa para voltar ao menu) ? ')
     if resp.lower() != 's':
         opcao = 9
         return opcao
+    else:
+        opcao = a
+        return opcao
+
+def estado():
+    print (f'Valor na memória: {memoria}')
 
 opcoes = {
     1: 'Somar',
@@ -17,21 +23,54 @@ memoria = 0
 opcao = 9
 
 while opcao == 9:
-    print('=' * 35)
-    print(f'Estado da memória: {memoria}')
-    print('=' * 35)
+    print ('=' * 35)
+    estado()
+    print ('=' * 35)
     for numero, descricao in opcoes.items():
-        print(f'{numero} - {descricao}')
-    print('=' * 35)
+        print (f'{numero} - {descricao}')
+    print ('=' * 35)
     opcao = int(input('Escolha a opção: '))
 
     if opcao not in opcoes:
         print('Opção inválida')
 
+    elif opcao == 0:
+        print ('Programa finalizado.')
+        break
+
     while opcao == 1:
-        num = float(input('Quanto deseja somar à memória? '))
-        memoria = memoria + num
-        """opcao = retornomenu()
-        if opcao == 1:
-            continue"""
-        
+        estado()
+        num = float(input('Quanto deseja somar ao valor na memória? '))
+        resultado = memoria + num
+        print (f'{memoria} + {num} = {resultado}')
+        memoria = resultado
+        opcao = retornomenu(1)
+    
+    while opcao == 2:
+        estado()
+        num = float(input('Quanto deseja subtrair do valor na memória? '))
+        resultado = memoria - num
+        print (f'{memoria} - {num} = {resultado}')
+        memoria = resultado
+        opcao = retornomenu(2)
+
+    while opcao == 3:
+        estado()
+        num = float(input('Por quanto deseja multiplicar o valor na memória? '))
+        resultado = memoria * num
+        print (f'{memoria} * {num} = {resultado}')
+        memoria = resultado
+        opcao = retornomenu(3)
+    
+    while opcao == 4:
+        estado()
+        num = float(input('Por quanto deseja dividir o valor na memória? '))
+        resultado = memoria / num
+        print (f'{memoria} / {num} = {resultado}')
+        memoria = resultado
+        opcao = retornomenu(4)
+
+    while opcao == 5:
+        memoria = 0
+        print ('Memória limpa \nVoltando ao menu')
+        opcao = 9
