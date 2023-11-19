926c68bac1602acc3255892e3dbb1b98fbdd2b44
otavio-alvxs <otavioalvescbp@gmail.com>
2023-11-19 13:32:34 -0300

    .

--- 2P/Estrutura de dados/Projeto01/projeto01.py
+++ 2P/Estrutura de dados/Projeto01/projeto01.py
@@ -18,18 +18,18 @@ def leitura(nome_arquivo: str):
 def soma_valores_arquivo():
     valores = leitura("entrada.txt")
     resultado = sum(valores)
-    print (resultado)
+    print ("Soma dos valores do arquivo: ", resultado)
 
 def maior_valor_arquivo():
     lista = leitura("entrada.txt")
-    print (max(lista))
+    print ("Maior valor do arquivo: ", max(lista))
 
 def multiplica_valores_arquivo():
     lista = leitura("entrada.txt")
     produto = lista[0]
     for numero in lista[1:]:
         produto *= numero
-    print (produto)
+    print ("Multiplicação dos valores do arquivo: ", produto)
 
 if __name__ == '__main__':
     soma_valores_arquivo()
