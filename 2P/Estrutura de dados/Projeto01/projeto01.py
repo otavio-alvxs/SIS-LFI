b444650590e996803429c5145ad326f9801df3c7
otavio-alvxs <otavioalvescbp@gmail.com>
2023-11-19 13:14:19 -0300

    projeto 01 ED

---
+++ 2P/Estrutura de dados/Projeto01/projeto01.py
@@ -0,0 +1,37 @@
+
+def leitura(nome_arquivo: str):
+    try:
+        lista = []
+        with open(nome_arquivo, 'r') as arquivo:
+            arquivo.seek(0)
+            for linha in arquivo:
+                linha = linha.strip()
+                if linha.isdigit():
+                    valor =  float(linha)
+                    lista.append(valor)
+            return lista
+    except Exception as e:
+        print("Houve uma falha na abertura do arquivo")
+        print(e)
+        return None
+
+def soma_valores_arquivo():
+    valores = leitura("entrada.txt")
+    resultado = sum(valores)
+    print (resultado)
+
+def maior_valor_arquivo():
+    lista = leitura("entrada.txt")
+    print (max(lista))
+
+def multiplica_valores_arquivo():
+    lista = leitura("entrada.txt")
+    produto = lista[0]
+    for numero in lista[1:]:
+        produto *= numero
+    print (produto)
+
+if __name__ == '__main__':
+    soma_valores_arquivo()
+    maior_valor_arquivo()
+    multiplica_valores_arquivo()
