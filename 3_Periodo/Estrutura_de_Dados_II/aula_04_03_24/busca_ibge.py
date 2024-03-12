6392836a3c12d796698d2f1134672a32e0f8fc06
otavio-alvxs <otavioalvescbp@gmail.com>
2024-03-12 13:40:13 -0300

    ibge att

--- 3_Periodo/Estrutura_de_Dados_II/aula_04_03_24/busca_ibge.py
+++ 3_Periodo/Estrutura_de_Dados_II/aula_04_03_24/busca_ibge.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from ibge import busca
+import ibge
 
 app = Flask(__name__)
 
@@ -7,18 +7,33 @@ app = Flask(__name__)
 def busca_nome():
     try:
         nome = request.args.get("nome")
-        response = busca(nome)
-        
-        # percorrer o retorno do busca e somar todas as ocorrências do nome
-        soma = 0
-        for valor in response[0]["res"]:
-            soma += valor.get("frequencia", 0)
-
-        mensagem = f"A soma de todas as ocorrências do nome Jose é {soma}"
-        return mensagem
-
+        response = ibge.busca(nome)
+        soma = ibge.calcula_ocorrencias(response)
+        objeto_retorno = {
+            "nome_procurado": nome,
+            "total_ocorrencias": soma
+        }  
+        return objeto_retorno
     except Exception as e:
         return f"Falha na rota /busca_nome: {e}"
-    
+
+@app.route("/frequencia_max")
+def frequencia_max():
+    try:
+        nome = request.args.get("nome")
+        json = ibge.busca(nome)
+        objeto = ibge.maior_frequencia(json)
+        return objeto
+    except Exception as e:
+        return f"Falha na rota /busca_nome: {e}"
+
+@app.route("/frequencia_min")
+def frequencia_min():
+    try:
+        nome = request.args.get("nome")
+        objeto = ibge.menor_frequencia(nome)
+        return objeto
+    except Exception as e:
+        return f"Falha na rota /frequencia_min: {e}"
 
 app.run(debug=True)
--- 3_Periodo/Estrutura_de_Dados_II/aula_04_03_24/ibge.py
+++ 3_Periodo/Estrutura_de_Dados_II/aula_04_03_24/ibge.py
@@ -5,5 +5,45 @@ def busca(nome):
     resposta = requests.get(url)
     return resposta.json()
 
-retorno = busca("Jose")
-print(retorno)
+def calcula_ocorrencias(json):
+    conteudo = json[0]
+    resposta = conteudo.get("res", [])
+    soma = []
+    for elemento in resposta:
+        frequencia = elemento.get("frequencia", 0)
+        soma.append(frequencia)
+    return sum(soma)
+
+def maior_frequencia(json):
+    conteudo = json[0]
+    resposta = conteudo.get("res", [])
+    maior_periodo = ""
+    maior_frequencia = 0
+    for elemento in resposta:
+        if elemento["frequencia"] > maior_frequencia:
+            maior_frequencia = elemento["frequencia"]
+            maior_periodo = elemento["periodo"]
+    objeto = {
+        "maior_frequencia" : maior_frequencia,
+        "periodo" : maior_periodo
+    }
+    return objeto
+
+def menor_frequencia(nome):
+    json = busca(nome)
+    conteudo = json[0]
+    resposta = conteudo.get("res", [])
+    print (resposta)
+    menor_periodo = ""
+    menor_frequencia = 999999999999
+    for elemento in resposta:
+        if elemento["frequencia"] < menor_frequencia:
+            menor_frequencia = elemento["frequencia"]
+            menor_periodo = elemento["periodo"]
+    objeto = {
+        f"Nome requisitado: {nome}\n"
+        "menor_frequencia" : menor_frequencia,
+        "periodo" : menor_periodo
+    }
+    return objeto
+
