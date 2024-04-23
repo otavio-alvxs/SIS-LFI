9bb56cd76f7bd18776ab41770c2609ce59d34242
otavio-alvxs <otavioalvescbp@gmail.com>
2024-04-22 23:14:08 -0300

    Projeto02

--- 3_Periodo/Estrutura_de_Dados_II/Projeto02/app.py
+++ 3_Periodo/Estrutura_de_Dados_II/Projeto02/app.py
@@ -1,16 +1,41 @@
-from flask import Flask, Response
+from flask import Flask, jsonify
 import search
 
 app = Flask(__name__)
 
+@app.route("/listar_cidades")
+def listar_cidades():
+    return jsonify(search.filtra_informacoes())
+
+@app.route("/insertion_sort")
+def insertion_sort():
+    json = search.remover_acentos()
+    json_ordenado, comparacoes, tempo_execucao = search.insertion_sort(json)
+    return jsonify({"json_ordenado": json_ordenado, "comparacoes": comparacoes, "tempo_execucao": tempo_execucao})
+
+@app.route("/selection_sort")
+def selection_sort():
+    json = search.remover_acentos()
+    json_ordenado, comparacoes, tempo_execucao = search.selection_sort(json)
+    return jsonify({"json_ordenado": json_ordenado, "comparacoes": comparacoes, "tempo_execucao": tempo_execucao})
+
+@app.route("/bubble_sort")
+def bubble_sort():
+    json = search.remover_acentos()
+    json_ordenado, comparacoes, tempo_execucao = search.bubble_sort(json)
+    return jsonify({"json_ordenado": json_ordenado, "comparacoes": comparacoes, "tempo_execucao": tempo_execucao})
+
 @app.route("/merge_sort")
 def merge_sort():
-    pass
+    json = search.remover_acentos()
+    json_ordenado, comparacoes, tempo_execucao = search.merge_sort(json)
+    return jsonify({"json_ordenado": json_ordenado, "comparacoes": comparacoes, "tempo_execucao": tempo_execucao})
 
-@app.route("/teste")
-def teste():
-    response = search.busca()
-    #response = search.filtra_informacoes(json)
-    return response
+@app.route("/quick_sort")
+def quick_sort():
+    json = search.remover_acentos()
+    json_ordenado, comparacoes, tempo_execucao = search.quick_sort(json, 0, len(json)-1)
+    return jsonify({"json_ordenado": json_ordenado, "comparacoes": comparacoes, "tempo_execucao": tempo_execucao})
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
--- 3_Periodo/Estrutura_de_Dados_II/Projeto02/search.py
+++ 3_Periodo/Estrutura_de_Dados_II/Projeto02/search.py
@@ -1,4 +1,6 @@
 import requests
+import unidecode
+import time
 
 def busca():
     url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios?view=nivelado"
@@ -11,3 +13,117 @@ def filtra_informacoes():
     resultado = [{k: d[k] for k in chaves_a_manter} for d in json]
     return resultado
 
+def remover_acentos():
+    json = filtra_informacoes()
+    for item in json:
+        item['municipio-nome'] = unidecode.unidecode(item['municipio-nome'])
+        item['UF-nome'] = unidecode.unidecode(item['UF-nome'])
+    return json
+
+def insertion_sort(json):
+    comp = 0
+    start_time = time.time()
+    for i in range(1, len(json)):
+        key = json[i]
+        j = i - 1
+        while j >= 0 and json[j]["municipio-nome"] > key["municipio-nome"]:
+            json[j + 1] = json[j]
+            j -= 1
+            comp += 1
+        json[j + 1] = key
+    end_time = time.time()
+    return json, comp, end_time - start_time
+
+def selection_sort(json):
+    comp = 0
+    start_time = time.time()
+    n = len(json)
+    for i in range(n):
+        menor_idx = i
+        for j in range(i+1, n):
+            if json[j]["municipio-nome"] < json[menor_idx]["municipio-nome"]:
+                menor_idx = j
+                comp += 1
+        json[i], json[menor_idx] = json[menor_idx], json[i]
+    end_time = time.time()
+    return json, comp, end_time - start_time
+
+def bubble_sort(json):
+    comp = 0
+    start_time = time.time()
+    n = len(json)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if json[j]["municipio-nome"] > json[j+1]["municipio-nome"]:
+                json[j], json[j+1] = json[j+1], json[j]
+                comp += 1
+    end_time = time.time()
+    return json, comp, end_time - start_time
+
+def merge_sort(json):
+    def merge_sort_helper(json, comp):
+        if len(json) > 1:
+            meio = len(json)//2
+            L = json[:meio]
+            R = json[meio:]
+
+            merge_sort_helper(L, comp)
+            merge_sort_helper(R, comp)
+
+            i = j = k = 0
+
+            while i < len(L) and j < len(R):
+                if L[i]["municipio-nome"] < R[j]["municipio-nome"]:
+                    json[k] = L[i]
+                    i += 1
+                else:
+                    json[k] = R[j]
+                    j += 1
+                k += 1
+                comp[0] += 1
+
+            while i < len(L):
+                json[k] = L[i]
+                i += 1
+                k += 1
+
+            while j < len(R):
+                json[k] = R[j]
+                j += 1
+                k += 1
+
+    comp = [0]
+    start_time = time.time()
+    merge_sort_helper(json, comp)
+    end_time = time.time()
+    return json, comp[0], end_time - start_time
+
+def particao(json, baixo, alto, comp):
+    i = (baixo-1)
+    pivo = json[alto]["municipio-nome"]
+
+    for j in range(baixo, alto):
+
+        if json[j]["municipio-nome"] <= pivo:
+
+            i = i+1
+            json[i], json[j] = json[j], json[i]
+            comp[0] += 1
+
+    json[i+1], json[alto] = json[alto], json[i+1]
+    return (i+1)
+
+def quick_sort(json, baixo, alto):
+    def quick_sort_helper(json, baixo, alto, comp):
+        if len(json) == 1:
+            return json
+        if baixo < alto:
+            pi = particao(json, baixo, alto, comp)
+            quick_sort_helper(json, baixo, pi-1, comp)
+            quick_sort_helper(json, pi+1, alto, comp)
+
+    comp = [0]
+    start_time = time.time()
+    quick_sort_helper(json, baixo, alto, comp)
+    end_time = time.time()
+    return json, comp[0], end_time - start_time
